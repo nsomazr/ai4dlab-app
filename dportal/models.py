41edf18213450f5857c3aef37e2edf752a93c028
nsomazr <zreuben815@gmail.com>
2023-03-12 21:55:45 +0300

    data portal

--- dportal/models.py
+++ dportal/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.relpath(__file__)))
@@ -9,11 +10,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.relpath(__file__)))
 class DPortal(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    banner = models.ImageField(max_length=100, blank=True,upload_to=os.path.join(BASE_DIR,'media/dportal'))
-    document = models.ImageField(max_length=100, blank=True,upload_to=os.path.join(BASE_DIR,'media/dportal'))
+    file = models.FileField(max_length=100, blank=True,upload_to=os.path.join(BASE_DIR,'dportal'))
     description = models.TextField()
-    body = models.TextField()
+    body = RichTextUploadingField(blank=True,null=True)
+    thematic_area = models.IntegerField(default=0)
     status = models.IntegerField(default=0)
+    publish = models.IntegerField(default=0)
     slug = models.SlugField(null=False, unique=True)
     publisher = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
@@ -22,7 +24,7 @@ class DPortal(models.Model):
         
 
     def __str__(self):
-        return self.title
+        return self.name
 
     def get_absolute_url(self):
         return reverse("data_detail", kwargs={"slug": self.slug}) 
--- dportal/views.py
+++ dportal/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import DPortal
 from .serializers import DPortalModelSerializer
+from .forms import DataForm
+from django.contrib import messages
+import pandas as pd
 # Create your views here.
 
 class DPortalAPIView(APIView):
@@ -19,6 +22,81 @@ class DPortalAPIView(APIView):
             return Response(serializer.data, status = 201)
         return Response(serializer.errors, status = 400)
 
-    def dportal(request):
+    def data_list(request):
+        data = DPortal.objects.all()
+        context = {'data':data}
+        return render(request, template_name='tools/data_list.html', context=context)
 
-        return render(request, template_name='tools/data-portal.html', context={})
+    def dportal(request):
+        data = DPortal.objects.filter(publish=1, status=1)
+        context = {"data": data}
+        return render(request, template_name='tools/data-portal.html', context=context)
+
+    def add_data(request):
+
+        if request.method == 'POST' and request.FILES['file']:
+
+            news_form = DataForm(request.POST,request.FILES)
+
+            if news_form.is_valid():
+                name  = request.POST['name']
+                description = request.POST['description']
+                body = request.POST['body']
+                file = request.FILES['file']
+                thematic_area= request.POST['thematic_area']
+                status = 1
+                slug = name.replace(' ','-').lower()
+                new_data = DPortal(name=name, description=description, body=body, file=file,thematic_area=thematic_area, status=status,publisher_id=request.session['user_id'], slug=slug)
+                get_objects = DPortal.objects.filter(name=name, status=1)
+                if get_objects:
+                    messages.success(request, "Data already exist." )
+                    data_form = DataForm()
+                    return render(request, template_name='tools/add_data.html', context={'data_form':data_form})
+                else:
+                    new_data.save()
+                    messages.success(request, "Data successful added." )
+                    return redirect('dportal:data-list')
+            else:
+                print(data_form.errors.as_data())
+                
+        data_form = DataForm()
+        return render(request, template_name='tools/add_data.html', context={'data_form':data_form})
+
+
+    def review_data(request,id):
+        data = DPortal.objects.get(id=id)
+        data_file = None
+        if str(data.file).endswith(".xlsx"):
+            data_file = pd.read_excel(data.file)
+        if str(data.file).endswith(".csv"):
+            data_file = pd.read_excel(data.file)
+        context = {'data':data, 'data_file':data_file.head(5).to_html()}
+        return render(request, template_name='tools/review_data.html', context=context)
+    
+    def read_data(request,slug):
+        data = DPortal.objects.get(slug=slug)
+        publisher = User.objects.get(id=data.publisher_id)
+        publisher_posts = DPortal.objects.filter(author_id=data.publisher_id).exclude(slug=slug)
+        context = {'data':data, 'publisher_posts':publisher_posts, 'publisher_posts':publisher_posts[:3]}
+        return render(request, template_name='tools/data_single.html', context=context)
+    
+    def view_data(request,id):
+        data = DPortal.objects.get(id=id)
+        context = {'data':data}
+        return render(request, template_name='tools/view_data.html', context=context)
+    
+    def publish_data(request,id):
+            data = DPortal.objects.get(id=id)
+            data.publish = 1
+            data.save()
+            return redirect('dportal:data-list')
+            
+    
+    def delete_data(request,id):
+        data = DPortal.objects.filter(id=id)
+        if data:
+            data.delete()
+            messages.success(request, "Data deleted." )
+            return redirect('dportal:data-list')
+        messages.success(request, "Data doesn't exist." )
+        return redirect('dportal:dportal-list')
