d1a1cb98026e4762eec2cbea57182bb27878c6b5
nsomazr <zreuben815@gmail.com>
2023-03-26 02:10:26 +0300

    conference and workshop

--- workshop/urls.py
+++ workshop/urls.py
@@ -4,13 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 app_name = "workshop"  
 
-urlpatterns = [path('', WorkshopAPIView.workshop, name="workshop"),
-               path('workshop-list/review-workshop/publish-workshop/<int:id>', WorkshopAPIView.publish_workshop, name="publish-workshop"),
-               path('workshop-list/review-workshop/<int:id>', WorkshopAPIView.review_workshop, name="review-workshop"),
-               path('workshop-list/view-workshop/<int:id>', WorkshopAPIView.view_workshop, name="view-workshop"),
-               path('workshop-list/deactivate-workshop/<int:id>', WorkshopAPIView.deactivate_workshop, name="deactivate-workshop"),
-               path('workshop-list/delete-workshop/<int:id>', WorkshopAPIView.delete_workshop, name="delete-workshop"),
-               path('add-workshop/', WorkshopAPIView.add_workshop, name="add-workshop"),
-               path('workshop-list/', WorkshopAPIView.workshop_list, name="workshop-list"),
+urlpatterns = [path('', WorkshopAPIView.workshop_22, name="workshop-22"),
+               path('workshop/22/sessions', WorkshopAPIView.workshop_22_sessions, name="workshop-22-sessions"),
+               path('workshop/22/speakers', WorkshopAPIView.workshop_22_sessions, name="workshop-22-speakers"),
+               path('workshop/22/organizers', WorkshopAPIView.workshop_22_organizers, name="workshop-22-organizers"),
                path('api/workshop/', WorkshopAPIView.as_view(), name="workshop-api")
                ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
